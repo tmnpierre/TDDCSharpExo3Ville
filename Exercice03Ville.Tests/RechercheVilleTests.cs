083d89af523825072b11561bea9ed79383457a23
tmnpierre <tomyn.pierre@gmail.com>
2024-01-25 16:12:16 +0100

    refactor: class RechercheVilleTests

--- Exercice03Ville.Tests/RechercheVilleTests.cs
+++ Exercice03Ville.Tests/RechercheVilleTests.cs
@@ -5,37 +5,34 @@ namespace Exercice03Ville.Tests
     [TestClass]
     public class RechercheVilleTests
     {
+        private RechercheVille rechercheVille;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            rechercheVille = new RechercheVille();
+            rechercheVille.AjouterVilles(new List<string> { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver",
+                                                            "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok",
+                                                            "Hong Kong", "Dubai", "Rome", "Istanbul" });
+        }
+
         [TestMethod]
         [ExpectedException(typeof(NotFoundException))]
         public void Search_TextLessThanTwoCharacters_ThrowsNotFoundException()
         {
-            var rechercheVille = new RechercheVille();
-
             rechercheVille.Rechercher("a");
         }
 
         [TestMethod]
         public void Search_TextTwoOrMoreCharacters_ReturnsMatchingCities()
         {
-            var rechercheVille = new RechercheVille();
-
-            rechercheVille.AjouterVilles(new List<string> { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", 
-                                                            "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", 
-                                                            "Hong Kong", "Dubai", "Rome", "Istanbul" });
-
             var resultats = rechercheVille.Rechercher("Va");
-
             CollectionAssert.AreEqual(new List<string> { "Valence", "Vancouver" }, resultats);
         }
+
         [TestMethod]
         public void Search_CaseInsensitiveSearch_ReturnsMatchingCities()
         {
-            var rechercheVille = new RechercheVille();
-
-            rechercheVille.AjouterVilles(new List<string> { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver",
-                                                            "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok",
-                                                            "Hong Kong", "Dubai", "Rome", "Istanbul" });
-
             var resultatsMinuscules = rechercheVille.Rechercher("va");
             var resultatsMajuscules = rechercheVille.Rechercher("VA");
             var resultatsMixtes = rechercheVille.Rechercher("Va");
@@ -49,28 +46,16 @@ namespace Exercice03Ville.Tests
         [TestMethod]
         public void Search_PartialCitySearch_ReturnsMatchingCities()
         {
-            var rechercheVille = new RechercheVille();
-
-            rechercheVille.AjouterVilles(new List<string> { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver",
-                                                            "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok",
-                                                            "Hong Kong", "Dubai", "Rome", "Istanbul" });
-
             var resultats = rechercheVille.Rechercher("ape");
-
             CollectionAssert.AreEqual(new List<string> { "Budapest" }, resultats);
         }
 
         [TestMethod]
         public void Search_Asterisk_ReturnsAllCities()
         {
-            var rechercheVille = new RechercheVille();
             var toutesLesVilles = new List<string> { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul" };
-            rechercheVille.AjouterVilles(toutesLesVilles);
-
             var resultats = rechercheVille.Rechercher("*");
-
             CollectionAssert.AreEquivalent(toutesLesVilles, resultats);
         }
-
     }
 }
