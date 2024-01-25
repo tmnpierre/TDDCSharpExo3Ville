ed4b06d7bdf96d31e9b5d05fe9bfdac97efb8ee7
tmnpierre <tomyn.pierre@gmail.com>
2024-01-25 15:04:27 +0100

    RED: Search_TextLessThanTwoCharacters_ThrowsNotFoundException

--- Exercice03Ville.Tests/RechercheVilleTests.cs
+++ Exercice03Ville.Tests/RechercheVilleTests.cs
@@ -1,11 +1,16 @@
+using Exercice03Ville.Core;
+
 namespace Exercice03Ville.Tests
 {
     [TestClass]
     public class RechercheVilleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Search_TextLessThanTwoCharacters_ThrowsNotFoundException()
         {
+            var rechercheVille = new RechercheVille();
+            var searchWord = "a";
+            rechercheVille.Rechercher(searchWord);
         }
     }
 }
